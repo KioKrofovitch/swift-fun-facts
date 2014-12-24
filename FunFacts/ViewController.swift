8b5c8762a7ec6dfba9de8b30c094372920318f35
Kelly Shuster <kelly.shuster@gmail.com>
2014-12-23 23:50:05 -0700

    First Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Kelly Shuster on 12/23/14.
+//  Copyright (c) 2014 KioDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFunFact() {
+        println("You pressed me!")
+    }
+
+}
+
